e406a2b25554d7e324be9c666d963c68a4cf217a
Filipp Zhinkin <filipp.zhinkin@jetbrains.com>
2025-02-26 19:54:46 -0500

    Add a benchmark

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,8 +2,8 @@ import kotlinx.benchmark.gradle.JvmBenchmarkTarget
 import org.jetbrains.kotlin.gradle.targets.js.dsl.ExperimentalWasmDsl
 
 plugins {
-    kotlin("multiplatform") version "2.0.20"
-    id("org.jetbrains.kotlinx.benchmark") version "0.4.12"
+    kotlin("multiplatform") version "2.1.0"
+    id("org.jetbrains.kotlinx.benchmark") version "0.4.13"
 }
 
 group = "org.example"
@@ -29,7 +29,8 @@ kotlin {
     sourceSets {
         commonMain {
             dependencies {
-                implementation("org.jetbrains.kotlinx:kotlinx-benchmark-runtime:0.4.12")
+                implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.6.2")
+                implementation("org.jetbrains.kotlinx:kotlinx-benchmark-runtime:0.4.13")
             }
         }
     }
---
+++ src/commonMain/kotlin/WhenOverObjects.kt
@@ -0,0 +1,74 @@
+package org.example
+
+import kotlinx.benchmark.Benchmark
+import kotlinx.benchmark.Blackhole
+import kotlinx.benchmark.Scope
+import kotlinx.benchmark.State
+import kotlinx.datetime.LocalDate
+import kotlin.random.Random
+
+class DayValue(f1: String, f2: String)
+
+fun get(date: LocalDate): DayValue {
+    return when (date) {
+        LocalDate(2024, 5, 22) -> DayValue("Workshop", "Day")
+        LocalDate(2024, 5, 23) -> DayValue("Conference", "Day 1")
+        LocalDate(2024, 5, 24) -> DayValue("Conference", "Day 2")
+        else -> DayValue("", "")
+    }
+}
+
+object Dates {
+    val day1 = LocalDate(2024, 5, 22)
+    val day2 = LocalDate(2024, 5, 23)
+    val day3 = LocalDate(2024, 5, 24)
+}
+
+fun getComparingToConstants(date: LocalDate): DayValue {
+    return when (date) {
+        Dates.day1 -> DayValue("Workshop", "Day")
+        Dates.day2 -> DayValue("Conference", "Day 1")
+        Dates.day3 -> DayValue("Conference", "Day 2")
+        else -> DayValue("", "")
+    }
+}
+
+fun getWithScopedConstants(date: LocalDate): DayValue {
+    val day1 = LocalDate(2024, 5, 22)
+    val day2 = LocalDate(2024, 5, 23)
+    val day3 = LocalDate(2024, 5, 24)
+    return when (date) {
+        day1 -> DayValue("Workshop", "Day")
+        day2 -> DayValue("Conference", "Day 1")
+        day3 -> DayValue("Conference", "Day 2")
+        else -> DayValue("", "")
+    }
+}
+
+@State(Scope.Benchmark)
+open class WhenOverObjects {
+    private val dates = Array<LocalDate>(30) {
+        LocalDate(2024, 5, it + 1)
+    }.apply { shuffle(Random(42)) }
+
+    @Benchmark
+    fun allocateDateInWhenClause(blackhole: Blackhole) {
+        dates.forEach {
+            blackhole.consume(get(it))
+        }
+    }
+
+    @Benchmark
+    fun createPreAllocatedConstants(blackhole: Blackhole) {
+        dates.forEach {
+            blackhole.consume(getComparingToConstants(it))
+        }
+    }
+
+    @Benchmark
+    fun createConstantsWithinFunction(blackhole: Blackhole) {
+        dates.forEach {
+            blackhole.consume(getWithScopedConstants(it))
+        }
+    }
+}
